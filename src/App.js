e4c20ba65a88946e962b00f23ca2cf42a49d3cc3
Matias <76888312+Matias-Garcia-dev@users.noreply.github.com>
2022-03-21 09:55:40 -0300

    Updated Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { NewNote } from './components/NewNote';
 import { Note } from './components/Note';
+import { Container, Paper } from '@material-ui/core';
 
 const App = ({ store }) => {
   const state = useSelector((state) => state.notes);
@@ -9,32 +10,34 @@ const App = ({ store }) => {
   };
 
   return (
-    <div>
-      <NewNote></NewNote>
-      <div>
-        all
-        <input
-          type='radio'
-          name='filter'
-          onChange={() => filterSelected('all')}
-        />
-        {'    '}
-        important
-        <input
-          type='radio'
-          name='filter'
-          onChange={() => filterSelected('important')}
-        />
-        {'   '}
-        not important
-        <input
-          type='radio'
-          name='filter'
-          onChange={() => filterSelected('not important')}
-        />
-      </div>
+    <Container>
+      <Paper>
+        <NewNote></NewNote>
+        <div>
+          all
+          <input
+            type='radio'
+            name='filter'
+            onChange={() => filterSelected('all')}
+          />
+          {'    '}
+          important
+          <input
+            type='radio'
+            name='filter'
+            onChange={() => filterSelected('important')}
+          />
+          {'   '}
+          not important
+          <input
+            type='radio'
+            name='filter'
+            onChange={() => filterSelected('not important')}
+          />
+        </div>
+      </Paper>
       <Note state={state}></Note>
-    </div>
+    </Container>
   );
 };
 
